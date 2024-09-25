573329a45341d8a04861faa6652ffd5882d356af
Shahinul99 <rumaisaabrar99@gmail.com>
2024-09-25 16:15:42 -0400

    added 1 part of js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('button clinking ')
+
+
+document.getElementById('btn-donate')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        
+
+        const newAmount = document.getElementById('Amount').value ;
+        console.log('newAmount');
+
+
+        if (NewAmount === '100'){
+            console.log ('you added Amount');
+            window.location.href = '/home.html';
+        }
+        else {
+            alert('plz only Amount');
+        }
+
+})
+
